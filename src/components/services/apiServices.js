80e82a8248731bc2219b271b3e60f859b5ce2cbf
bot4ka <trololowka.t.r.77@gmail.com>
2023-06-05 09:35:17 +0300

    credentials

--- src/components/services/apiServices.js
+++ src/components/services/apiServices.js
@@ -3,10 +3,7 @@ export class apiServices {
   static getApiData() {
     return axios
       .get("https://furniturebackendapp.herokuapp.com/", {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-        },
+        credentials: "include",
       })
       .then((response) => {
         return response.data;
